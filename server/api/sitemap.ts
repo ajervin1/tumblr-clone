459534149d0222690e11b52ecdce701f54ea9474
ajervin1 <anthony.ervin100@gmail.com>
2023-12-08 20:22:06 -0700

    Sitemap fix

--- server/api/sitemap.ts
+++ server/api/sitemap.ts
@@ -1,18 +1,27 @@
 import { serverSupabaseClient } from "#supabase/server";
-
+// @ts-nocheck
 export default defineSitemapEventHandler(async (event) => {
 	const client = await serverSupabaseClient(event);
 	
 	let { data: tiktoks, error } = await client
 		.from('tiktoks')
 		.select('*');
-	console.log(tiktoks)
-	const routes = tiktoks.map(tiktok => {
-		// @ts-nocheck
-		return {
-			loc: `/tiktoks/${tiktok.id}`,
+	
+	let routes = []
+	tiktoks.forEach(tiktok => {
+		routes.push( {
+			// @ts-ignore
+			loc: `/tiktoks/${tiktok.desc}-${tiktok.id}`,
 			lastmod: new Date()
-		}
+		})
 	});
+	let usernames = tiktoks.map(tiktok => tiktok.username);
+	const usernameSet = new Set(usernames);
+	usernameSet.forEach(username => {
+		routes.push({
+			loc: `/tiktoks/user/${username}}`,
+			lastmod: new Date()
+		})
+	})
 	return routes;
 });
