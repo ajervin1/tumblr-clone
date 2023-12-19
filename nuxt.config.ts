81c69c115b04ac4b4f17e6dbbd8e689b8ef638df
ajervin1 <anthony.ervin100@gmail.com>
2023-12-19 02:18:20 -0700

    Adding Google Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
 		apiSecret: '123',
 		// Keys within public are also exposed client-side
 		public: {
+			gtagId: "G-YV2S4077XY",
 			baseUrl: process.env.BASE_URL ?? "http://localhost:3000"
 		}
 	},
